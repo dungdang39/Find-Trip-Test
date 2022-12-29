f6bedac13336db906760f955473058d3c6657fb1
sintaeho <sth0701@icloud.com>
2022-12-29 16:35:09 +0900

    Edit README

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="Home">
       <p className="subTitle">유형 테스트로 알아보는</p>
-      <h1>지금 나에게 필요한 여행지는?</h1>
+      <h1>나만을 위한 국내 여행지는?</h1>
       <div className="imgWrap">
         <img src={process.env.PUBLIC_URL + "assets/back_img.png"} />
       </div>
